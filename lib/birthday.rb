3b137aa602a924e813f61f31d10a51fe9f771a3b
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-26 17:03:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,10 +4,10 @@
    "Amanda" => 27
  }
 
-def birthday_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age| 
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
 
-puts birthday_birthday(birthday_kids)
+puts happy_birthday(birthday_kids)
