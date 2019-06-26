07a8529962bb6974420b6fff0cf697371d9f9887
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-26 17:00:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,5 +1,9 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+ }
+
+def birthday_message(birthday_kids)
+  birthday_kids.each do |kid| 
+    puts "Happy Birthday #{kid}, "
